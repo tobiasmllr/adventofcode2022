8f8173d275bcdd097f9d2f0ed062e418a4a79fc0
Tobias Mueller <to.mueller@gmail.com>
2022-12-06 20:30:09 -0800

    simplified

--- day6/day6.py
+++ day6/day6.py
@@ -1,19 +1,14 @@
 import os
-import numpy as np
 
-# load txt file as list:
 with open(os.path.join(os.path.dirname(__file__), "input.txt")) as f:
     input = f.readlines()
 input = input[0]
 
 def find_seq(seq_len):
-    for i in range(0,len(input)):
-        if i > seq_len - 1:
-            seq=input[i-seq_len:i]
-            if len(set(seq)) == seq_len:
-                return i
+    for i in range(seq_len - 1, len(input)):
+        seq=input[i-seq_len:i]
+        if len(set(seq)) == seq_len:
+            return i
 
 print(f'answer 1: {find_seq(4)}')
-
-### part 2
-print(f'answer 1: {find_seq(14)}')
+print(f'answer 2: {find_seq(14)}')
